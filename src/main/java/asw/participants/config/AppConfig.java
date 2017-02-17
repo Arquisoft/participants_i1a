9856a409ea99ef6c2dfa7e0e8304955c1f0b6f1a
jgm <juliangm96@gmail.com>
2017-02-17 19:24:33 +0100

    Added method for the Content Negotiation.

--- src/main/java/asw/participants/config/AppConfig.java
+++ src/main/java/asw/participants/config/AppConfig.java
@@ -3,16 +3,30 @@ package asw.participants.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import asw.participants.persistence.UserService;
 import asw.participants.persistence.UserServiceFakeImpl;
 
 @Configuration
-public class AppConfig {
+//@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public UserService userService() {
 		return new UserServiceFakeImpl();
 	}
 	
+	
+	@Override	
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+	configurer.favorPathExtension(false).
+	        favorParameter(true).
+	        defaultContentType(MediaType.APPLICATION_JSON).
+	        mediaType("html", MediaType.APPLICATION_XHTML_XML);
+	  }
+	  
 }
