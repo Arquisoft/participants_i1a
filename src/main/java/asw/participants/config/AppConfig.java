3f9d28e1d0ea1d751fdd549d43eaf6f66957f75f
jgm <juliangm96@gmail.com>
2017-02-23 10:35:06 +0100

    Deleted WEB-INF folder and Spring Security dependencies in pom.xml

--- src/main/java/asw/participants/config/AppConfig.java
+++ src/main/java/asw/participants/config/AppConfig.java
@@ -3,29 +3,16 @@ package asw.participants.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import asw.participants.persistence.UserService;
 import asw.participants.persistence.UserServiceFakeImpl;
 
 @Configuration
-//@EnableWebMvc
-public class AppConfig extends WebMvcConfigurerAdapter {
+public class AppConfig{
 
 	@Bean
 	public UserService userService() {
 		return new UserServiceFakeImpl();
-	}
+	}	
 	
-	
-	@Override	
-	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-			configurer.favorPathExtension(false)
-			        .favorParameter(true)
-			        .defaultContentType(MediaType.APPLICATION_JSON)
-			        .mediaType("html", MediaType.APPLICATION_XHTML_XML);
-	  }
-	  
 }
